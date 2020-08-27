fac9989c8ff409b54de7ef0a14c69bf76025330b
Quentin Pla <quentin-pla@hotmail.fr>
2020-08-27 15:42:45 +0200

    Updates

---
+++ precache-manifest.97d8bbc6454cddcf2e4eae6971a09b11.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bfe1f77a43dfdb97ad0f9d31bbd207",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "ddd4377390acee7a7455",
+    "url": "/personal-website/static/css/2.ceb06015.chunk.css"
+  },
+  {
+    "revision": "9b7d31bb1c243aaa5433",
+    "url": "/personal-website/static/css/main.9df22f13.chunk.css"
+  },
+  {
+    "revision": "ddd4377390acee7a7455",
+    "url": "/personal-website/static/js/2.382badc5.chunk.js"
+  },
+  {
+    "revision": "30bd33cf088f2494cacd5519e622c912",
+    "url": "/personal-website/static/js/2.382badc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7d31bb1c243aaa5433",
+    "url": "/personal-website/static/js/main.942ad451.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  },
+  {
+    "revision": "4fbd2bf6049e1f316ba29fe31b470d67",
+    "url": "/personal-website/static/media/Avenir-Black.4fbd2bf6.otf"
+  },
+  {
+    "revision": "cc003b39f6eb16313ce7522a803de052",
+    "url": "/personal-website/static/media/Avenir-Heavy.cc003b39.otf"
+  },
+  {
+    "revision": "2e2fb155893983642017d6ac9e9af2e3",
+    "url": "/personal-website/static/media/Avenir-Light.2e2fb155.otf"
+  },
+  {
+    "revision": "11f78470670c1bac626c17f814c9530b",
+    "url": "/personal-website/static/media/Avenir-Medium.11f78470.otf"
+  },
+  {
+    "revision": "8b70fa90596a7eccad9a04dce512b580",
+    "url": "/personal-website/static/media/Avenir-Roman.8b70fa90.otf"
+  }
+]);
