8d89859d8ac8705c8f01c1b6ee9de9c7227240f7
Quentin Pla <quentin-pla@hotmail.fr>
2020-08-27 18:56:09 +0200

    Updates

---
+++ precache-manifest.1ce69715f8de8d5f49373cf80e8a000c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7e88fec8e7dddc0b437e96df70be6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd4377390acee7a7455",
+    "url": "/static/css/2.ceb06015.chunk.css"
+  },
+  {
+    "revision": "a57319ad53ea8b4e11ce",
+    "url": "/static/css/main.6251049a.chunk.css"
+  },
+  {
+    "revision": "ddd4377390acee7a7455",
+    "url": "/static/js/2.382badc5.chunk.js"
+  },
+  {
+    "revision": "30bd33cf088f2494cacd5519e622c912",
+    "url": "/static/js/2.382badc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57319ad53ea8b4e11ce",
+    "url": "/static/js/main.5c91beb3.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "4fbd2bf6049e1f316ba29fe31b470d67",
+    "url": "/static/media/black.4fbd2bf6.otf"
+  },
+  {
+    "revision": "cc003b39f6eb16313ce7522a803de052",
+    "url": "/static/media/heavy.cc003b39.otf"
+  },
+  {
+    "revision": "2e2fb155893983642017d6ac9e9af2e3",
+    "url": "/static/media/light.2e2fb155.otf"
+  },
+  {
+    "revision": "11f78470670c1bac626c17f814c9530b",
+    "url": "/static/media/medium.11f78470.otf"
+  },
+  {
+    "revision": "8b70fa90596a7eccad9a04dce512b580",
+    "url": "/static/media/roman.8b70fa90.otf"
+  }
+]);
